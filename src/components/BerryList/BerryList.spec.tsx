ce6624cba466cf6164fcc74ef2a73620bc6cd8fb
hmdnprks <hamdan.prakoso@mail.ugm.ac.id>
2024-01-11 01:35:50 +0700

    test: add test case for BerryList package

---
+++ src/components/BerryList/BerryList.spec.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import BerryList from './BerryList';
+import '@testing-library/jest-dom';
+import { BerryItemResult } from '@component/interfaces/berry';
+
+describe('BerryList', () => {
+  const mockBerries: BerryItemResult[] = [
+    { id: '1', name: 'Cheri', imageUrl: '/cheri.png', firmness: 'very-soft', url: 'https://pokeapi.co/api/v2/berry/1/' },
+    { id: '2', name: 'Chesto', imageUrl: '/chesto.png', firmness: 'very-soft', url: 'https://pokeapi.co/api/v2/berry/2/' },
+  ];
+
+  it('renders the list of berries', () => {
+    render(<BerryList berries={mockBerries} setSelectedBerry={() => { }} />);
+    const berryElements = screen.getAllByRole('img');
+    expect(berryElements).toHaveLength(mockBerries.length);
+  });
+
+  it('calls setSelectedBerry when a berry is clicked', () => {
+    const mockSetSelectedBerry = jest.fn();
+    render(<BerryList berries={mockBerries} setSelectedBerry={mockSetSelectedBerry} />);
+    const firstBerry = screen.getByAltText('Cheri');
+    fireEvent.click(firstBerry);
+    expect(mockSetSelectedBerry).toHaveBeenCalledWith(mockBerries[0]);
+  });
+
+  it('highlights the selected berry', () => {
+    const { rerender } = render(<BerryList berries={mockBerries} setSelectedBerry={() => { }} />);
+    const firstBerry = screen.getByAltText('Cheri');
+    fireEvent.click(firstBerry);
+    rerender(<BerryList berries={mockBerries} setSelectedBerry={() => { }} />);
+    expect(firstBerry.parentElement).toHaveClass('bg-red-300');
+  });
+
+  it('displays the correct alt text for each berry image', () => {
+    render(<BerryList berries={mockBerries} setSelectedBerry={() => { }} />);
+    const firstBerryName = screen.getByAltText('Cheri');
+    const secondBerryName = screen.getByAltText('Chesto');
+    expect(firstBerryName).toBeInTheDocument();
+    expect(secondBerryName).toBeInTheDocument();
+  });
+});
