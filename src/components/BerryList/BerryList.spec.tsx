2a91ebb59cdb5ff1b1294d479bf7d272aeed5257
hmdnprks <hamdan.prakoso@mail.ugm.ac.id>
2024-01-11 15:29:18 +0700

    fix: add skeleton on berries and UT

--- src/components/BerryList/BerryList.spec.tsx
+++ src/components/BerryList/BerryList.spec.tsx
@@ -6,34 +6,30 @@ import { BerryItemResult } from '@component/interfaces/berry';
 
 describe('BerryList', () => {
   const mockBerries: BerryItemResult[] = [
-    { id: '1', name: 'Cheri', imageUrl: '/cheri.png', firmness: 'very-soft', url: 'https://pokeapi.co/api/v2/berry/1/' },
-    { id: '2', name: 'Chesto', imageUrl: '/chesto.png', firmness: 'very-soft', url: 'https://pokeapi.co/api/v2/berry/2/' },
+    {
+      id: '1',
+      name: 'Cheri',
+      imageUrl: '/cheri.png',
+      firmness: 'very-soft',
+      url: 'https://pokeapi.co/api/v2/berry/1/',
+    },
+    {
+      id: '2',
+      name: 'Chesto',
+      imageUrl: '/chesto.png',
+      firmness: 'very-soft',
+      url: 'https://pokeapi.co/api/v2/berry/2/',
+    },
   ];
 
   it('renders the list of berries', () => {
-    render(<BerryList berries={mockBerries} setSelectedBerry={() => { }} />);
+    render(<BerryList berries={mockBerries} isLoading={false} setSelectedBerry={() => {}} />);
     const berryElements = screen.getAllByRole('img');
     expect(berryElements).toHaveLength(mockBerries.length);
   });
 
-  it('calls setSelectedBerry when a berry is clicked', () => {
-    const mockSetSelectedBerry = jest.fn();
-    render(<BerryList berries={mockBerries} setSelectedBerry={mockSetSelectedBerry} />);
-    const firstBerry = screen.getByAltText('Cheri');
-    fireEvent.click(firstBerry);
-    expect(mockSetSelectedBerry).toHaveBeenCalledWith(mockBerries[0]);
-  });
-
-  it('highlights the selected berry', () => {
-    const { rerender } = render(<BerryList berries={mockBerries} setSelectedBerry={() => { }} />);
-    const firstBerry = screen.getByAltText('Cheri');
-    fireEvent.click(firstBerry);
-    rerender(<BerryList berries={mockBerries} setSelectedBerry={() => { }} />);
-    expect(firstBerry.parentElement).toHaveClass('bg-red-300');
-  });
-
   it('displays the correct alt text for each berry image', () => {
-    render(<BerryList berries={mockBerries} setSelectedBerry={() => { }} />);
+    render(<BerryList berries={mockBerries} isLoading={false} setSelectedBerry={() => {}} />);
     const firstBerryName = screen.getByAltText('Cheri');
     const secondBerryName = screen.getByAltText('Chesto');
     expect(firstBerryName).toBeInTheDocument();
@@ -41,8 +37,34 @@ describe('BerryList', () => {
   });
 
   it('does not render any berries when berries array is empty', () => {
-    render(<BerryList berries={[]} setSelectedBerry={() => { }} />);
+    render(<BerryList berries={[]} isLoading={false} setSelectedBerry={() => {}} />);
     const berryElements = screen.queryAllByRole('img');
     expect(berryElements).toHaveLength(0);
+  });
+
+  it('calls setSelectedBerry when a berry is clicked', () => {
+    const mockSetSelectedBerry = jest.fn();
+    render(
+      <BerryList berries={mockBerries} isLoading={false} setSelectedBerry={mockSetSelectedBerry} />,
+    );
+    const firstBerry = screen.getByAltText('Cheri');
+    fireEvent.click(firstBerry);
+    expect(mockSetSelectedBerry).toHaveBeenCalledWith(mockBerries[0]);
+  });
+
+  it('highlights the selected berry', () => {
+    const { rerender } = render(
+      <BerryList berries={mockBerries} isLoading={false} setSelectedBerry={() => {}} />,
+    );
+    const firstBerry = screen.getByAltText('Cheri');
+    fireEvent.click(firstBerry);
+    rerender(<BerryList berries={mockBerries} isLoading={false} setSelectedBerry={() => {}} />);
+    expect(firstBerry.parentElement).toHaveClass('bg-red-300');
+  });
+
+  it('renders the skeleton when isLoading is true', () => {
+    render(<BerryList berries={[]} isLoading={true} setSelectedBerry={() => {}} />);
+    const skeletonElements = screen.getAllByTestId('skeleton');
+    expect(skeletonElements).toHaveLength(10);
   });
 });
--- src/components/BerryList/BerryList.tsx
+++ src/components/BerryList/BerryList.tsx
@@ -5,9 +5,10 @@ import { BerryItemResult } from '@component/interfaces/berry';
 interface BerryListProps {
   berries: BerryItemResult[];
   setSelectedBerry: (berry: BerryItemResult) => void;
+  isLoading: boolean;
 }
 
-const BerryList: React.FC<BerryListProps> = ({ berries, setSelectedBerry }) => {
+const BerryList: React.FC<BerryListProps> = ({ berries, setSelectedBerry, isLoading }) => {
   const [selectedBerryId, setSelectedBerryId] = useState<string | null>(null);
 
   const handleSelectBerry = (berry: BerryItemResult) => {
@@ -15,11 +16,33 @@ const BerryList: React.FC<BerryListProps> = ({ berries, setSelectedBerry }) => {
     setSelectedBerry(berry);
   };
 
+  const BerrySkeleton = () => (
+    <div className="flex overflow-x-auto gap-2 p-2 rounded-full border">
+      {Array.from(Array(10).keys()).map((i) => (
+        <div
+          className="flex-none mx-2 p-1 rounded-full animate-pulse bg-gray-300 w-10 h-10"
+          data-testid="skeleton"
+          key={`skeleton-${i}`}
+        />
+      ))}
+    </div>
+  );
+
+  if (isLoading) {
+    return <BerrySkeleton />;
+  }
+
   if (berries?.length > 0) {
     return (
       <div className="flex overflow-x-auto p-2 rounded-full border">
         {berries.map((berry) => (
-          <div className={`flex-none mx-2 p-1 rounded-lg ${selectedBerryId === berry.id ? 'bg-red-300' : ''}`} key={berry.id} onClick={() => handleSelectBerry(berry)}>
+          <div
+            className={`flex-none mx-2 p-1 rounded-lg ${
+              selectedBerryId === berry.id ? 'bg-red-300' : ''
+            }`}
+            key={berry.id}
+            onClick={() => handleSelectBerry(berry)}
+          >
             <img alt={berry.name} className="w-10 h-10 object-cover" src={berry.imageUrl} />
           </div>
         ))}
