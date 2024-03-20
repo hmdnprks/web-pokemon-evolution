2468008a1ebacbd417a8a0fe7d03c6c6cffaf320
hmdnprks <hamdan.prakoso@mail.ugm.ac.id>
2024-03-20 09:39:55 +0700

    feat(components): add information icon

---
+++ src/components/Icons/Information/InformationIcon.tsx
@@ -0,0 +1,26 @@
+interface IconProps {
+  className?: string;
+}
+
+const InformationIcon = (props: IconProps) => (
+  <svg
+    className={props.className}
+    fill="none"
+    stroke="currentColor"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    strokeWidth="2"
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <circle cx="12" cy="12" r="10" />
+    <line x1="12" x2="12" y1="16" y2="12" />
+    <line x1="12" x2="12.01" y1="8" y2="8" />
+  </svg>
+);
+
+InformationIcon.defaultProps = {
+  className: '',
+};
+
+export default InformationIcon;
