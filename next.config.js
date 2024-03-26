84b08a83eef36014a966f234d1133ee5a6ddb1bb
hmdnprks <hamdan.prakoso@mail.ugm.ac.id>
2024-03-26 14:52:12 +0700

    build: update next config with remote patterns for image

--- next.config.js
+++ next.config.js
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
+        port: '',
+      },
+    ],
+  },
+};
 
 module.exports = nextConfig;
 
