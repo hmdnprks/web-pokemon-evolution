c332c48a635e54acc27249f261284edd12ecad45
hmdnprks <hamdan.prakoso@mail.ugm.ac.id>
2024-03-26 14:51:22 +0700

    fix(api): return image url berries fron enumerated url

--- src/app/api/berry/route.ts
+++ src/app/api/berry/route.ts
@@ -1,24 +1,26 @@
-import { BerryListAPIResponse, BerrySingleAPIResponse, ItemAPIResponse } from '@component/interfaces/berry';
+import { BerryListAPIResponse, BerrySingleAPIResponse } from '@component/interfaces/berry';
 
 export async function GET(request: Request) {
   const res = await fetch(`${process.env.API_URL}/berry`);
   const data: BerryListAPIResponse = await res.json();
 
-  const berryListWithDetails = await Promise.all(data.results.map(async (berry: any) => {
-    const berryId = berry.url.split('/')[6];
-    const berryRes = await fetch(`${process.env.API_URL}/berry/${berryId}`);
-    const berryData: BerrySingleAPIResponse = await berryRes.json();
+  const berryListWithDetails = await Promise.all(
+    data.results.map(async (berry: any) => {
+      const berryId = berry.url.split('/')[6];
+      const berryRes = await fetch(`${process.env.API_URL}/berry/${berryId}`);
+      const berryData: BerrySingleAPIResponse = await berryRes.json();
+      const berryName = berryData.item.name;
 
-    const itemRes = await fetch(`${process.env.API_URL}/item/${berryData.item.url.split('/')[6]}`);
-    const itemData: ItemAPIResponse = await itemRes.json();
+      const imageBerry = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/${berryName}.png`;
 
-    return {
-      ...berry,
-      id: berryId,
-      firmness: berryData.firmness.name,
-      imageUrl: itemData.sprites.default
-    };
-  }));
+      return {
+        ...berry,
+        id: berryId,
+        firmness: berryData.firmness.name,
+        imageUrl: imageBerry,
+      };
+    }),
+  );
 
   return Response.json({ ...data, results: berryListWithDetails });
 }
