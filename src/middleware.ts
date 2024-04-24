be5ef0e6b05558f04896a16f9752444719d51631
hmdnprks <hamdan.prakoso@mail.ugm.ac.id>
2024-04-24 19:25:54 +0700

    fix(middleware): fix middleware to improve security

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,7 @@ export function middleware(request: NextRequest) {
   const nonce = Buffer.from(crypto.randomUUID()).toString('base64');
   const cspHeader = `
     default-src 'self';
-    script-src 'self' 'nonce-${nonce}' 'strict-dynamic' https: http: 'unsafe-inline' ${
+    script-src 'self' 'nonce-${nonce}' https: http: 'unsafe-inline' ${
   process.env.NODE_ENV === 'production' ? '' : `'unsafe-eval'`
 };
     style-src 'self' 'unsafe-inline';
