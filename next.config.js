ba22380a5a70e17a53519c230a0daa296146f346
hmdnprks <hamdan.prakoso@mail.ugm.ac.id>
2024-04-25 10:33:43 +0700

    chore: update cors config

--- next.config.js
+++ next.config.js
@@ -20,6 +20,18 @@ const nextConfig = {
           },
         ],
       },
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: 'https://hamdan.id' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT' },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'Accept, Accept-Version, Content-Length, Content-Type, Date',
+          },
+        ],
+      },
     ];
   },
 };
